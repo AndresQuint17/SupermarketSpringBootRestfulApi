3ca18d3888ed304419adfad621c75c8f490a2b8f
AndresQuint17 <78116310+AndresQuint17@users.noreply.github.com>
2023-10-02 10:23:37 -0500

    First version of the supermarket API

---
+++ src/main/java/com/platzi/market/domain/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.platzi.market.domain.repository;
+
+import com.platzi.market.domain.Category;
+
+import java.util.List;
+
+public interface CategoryRepository {
+
+    List<Category> getAll();
+}
